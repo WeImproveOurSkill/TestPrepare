c96c763b25d5ad7f1193e90979626b3597fa027d
김민수 <kimminsoo213@dankoook.ac.kr>
2025-02-03 15:19:44 +0900

    <Backend - java> <(User)> :  set default user table
    set default user table and definite user entity

---
+++ be/src/main/java/com/example/be/common/domain/user/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.be.common.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    private String oauth2Id;
+
+    //    private String email;
+    private String role;
+
+    private String provider;
+    private String providerId;
+}
