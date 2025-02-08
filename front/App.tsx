fc07f5e7f641a14e6e97646509ed21de2ccbd8a9
benidene <dlgusehd09@naver.com>
2025-02-08 10:20:09 +0900

    <remove> : 주석 제거

--- front/App.tsx
+++ front/App.tsx
@@ -1,50 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import {
-  // SafeAreaView,
-  // StyleSheet,
-  // Text,
-  // TextInput,
-  // View,
-} from 'react-native';
 import AuthStackNavigator from './src/navigation/AuthStackNavigator';
-
 
 function App(): React.JSX.Element {
 
   return (
     <NavigationContainer>
-      {/* <SafeAreaView style={styles.container}>
-        <View style={styles.viewContainer}>
-          <Text>안녕</Text>
-          <TextInput style={styles.textContainer}/>
-        </View>
-      </SafeAreaView> */}
       <AuthStackNavigator />
     </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: 'red',
-//     flex: 1,
-//     // alignItems: 'center',
-//     // justifyContent: 'center',
-//     // height: '100%',
-//   },
-//   viewContainer: {
-//     backgroundColor: 'yellow',
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   textContainer: {
-//     backgroundColor: 'blue',
-//     // flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 
 export default App;
