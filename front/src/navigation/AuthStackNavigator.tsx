4b0a10e0aac14f0cccecdad0e5df7fad3d8ee012
benidene <dlgusehd09@naver.com>
2025-02-18 01:52:08 +0900

    #14 <fe> <feat> : 카카오 로그인 기능 및 버튼 분리

--- front/src/navigation/AuthStackNavigator.tsx
+++ front/src/navigation/AuthStackNavigator.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { authNavigation } from '../constants';
 import { createStackNavigator } from '@react-navigation/stack';
 import AuthHomeScreen from '../screens/oauth/AuthHomeScreen';
+import examScreen from '../screens/exam/ExamScreen';
+import StudyScreen from '../screens/study/StudyScreen';
+import QuizScreen from '../screens/quiz/QuizScreen';
 import BottomTabNavigator from './BottomTabNavigator';
 
 
 export type AuthStackParamList = {
   AuthHome: undefined;
+  Login: undefined;
   Home: undefined;
   Exam: undefined;
   Study: undefined;
   Quiz: undefined;
+  BottomTabNavigator: undefined;
 }
 
 const Stack = createStackNavigator<AuthStackParamList>();
@@ -29,6 +34,18 @@ function AuthStackNavigator() {
       <Stack.Screen
         name={authNavigation.HOME}
         component={BottomTabNavigator}
+        />
+      <Stack.Screen
+        name={authNavigation.EXAM}
+        component={examScreen}
+        />
+      <Stack.Screen
+        name={authNavigation.STUDY}
+        component={StudyScreen}
+        />
+      <Stack.Screen
+        name={authNavigation.QUIZ}
+        component={QuizScreen}
         />
     </Stack.Navigator>
   );
