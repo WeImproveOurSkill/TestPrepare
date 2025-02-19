bf29532b5a6eecb59530e6e6ae029387dd719b76
benidene <dlgusehd09@naver.com>
2025-02-19 23:58:32 +0900

    <fe> <feat> : 최상위 컴포넌트인 app.tsx에 QueryClient 적용

--- front/App.tsx
+++ front/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStackNavigator from './src/navigation/AuthStackNavigator';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { StatusBar } from 'react-native';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      // 기본 캐시 시간 설정
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+    },
+  },
+});
 
 
 function App(): React.JSX.Element {
 
   return (
     // <StatusBar /> // 다크모드 적용시키기
+    <QueryClientProvider client={queryClient}>
       <NavigationContainer>
         <AuthStackNavigator />
       </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
