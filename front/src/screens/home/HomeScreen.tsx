d1a04de96f5004ea3cf9d484c3df0bf0b656cb6b
benidene <dlgusehd09@naver.com>
2025-02-24 22:51:54 +0900

    <fe> <feat> : 헤더 컴포넌트 분리로 인한 헤더 관련 코드 제거

--- front/src/screens/home/HomeScreen.tsx
+++ front/src/screens/home/HomeScreen.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Pressable, useWindowDimensions } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 import { StackScreenProps } from '@react-navigation/stack';
 import { AuthStackParamList } from '../../navigation/AuthStackNavigator';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import BookView from './components/BookView';
 import { authNavigation } from '../../constants';
 import { colors } from '../../constants/colors';
@@ -15,8 +14,7 @@ const HomeScreen = ({navigation}:AuthHomeScreenProps) => {
   const {theme} = useThemeStore();
   const styles = styling(theme);
   const isTablet = DeviceInfo.isTablet();
-
-  const insets = useSafeAreaInsets();
+  const { width } = useWindowDimensions();
 
   const handleSelectLicense = () => {
     navigation.navigate(authNavigation.AUTH_HOME);
@@ -27,16 +25,8 @@ const HomeScreen = ({navigation}:AuthHomeScreenProps) => {
   // 조회를 하게 되면 과목 고유 ID(subjectId: Long), 과목 이름(subjectName: String)이 나오게 되고 과목 고유 ID(subjectId)를 통해 문제를 조회하여 사용자에게 보여줌
 
   return (
-    <View style={[styles.container, {
-        paddingTop: insets.top,
-        paddingLeft: insets.left,
-        paddingRight: insets.right,
-      }]}>
-        <View style={styles.header}>
-          <Text style={styles.title}>기한82</Text>
-          <Text style={styles.loginButton}>로그인</Text>
-        </View>
-        <View style={!isTablet ? styles.container : styles.tabletContainer}>
+    <View style={styles.container}>
+        <View style={isTablet && width >= 600 ? styles.tabletContainer : styles.contentContainer}>
           <View style={styles.testLayout}>
           {title ? (
             <BookView title={title} coverColor={title} navigation={navigation} />
@@ -46,20 +36,6 @@ const HomeScreen = ({navigation}:AuthHomeScreenProps) => {
             </Pressable>
           )}
           </View>
-            {/* <View style={ !isTablet && styles.bottomNav }>
-              <Pressable style={ !isTablet ? styles.navItem : styles.navItemTablet }>
-                <Text style={styles.navText}>홈</Text>
-              </Pressable>
-              <Pressable style={ !isTablet ? styles.navItem : styles.navItemTablet }>
-                <Text style={styles.navText}>GPT 기록</Text>
-              </Pressable>
-              <Pressable style={ !isTablet ? styles.navItem : styles.navItemTablet }>
-                <Text style={styles.navText}>오답노트</Text>
-              </Pressable>
-              <Pressable style={ !isTablet ? styles.navItem : styles.navItemTablet }>
-                <Text style={styles.navText}>마이페이지</Text>
-              </Pressable>
-            </View> */}
         </View>
     </View>
   );
@@ -70,33 +46,16 @@ const styling = (theme: themeMode) => StyleSheet.create({
     flex: 1,
     backgroundColor: colors[theme].WHITE,
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 12,
-    paddingHorizontal: 20,
-    backgroundColor: colors[theme].WHITE,
-    borderBottomColor: colors[theme].WHITE,
-    borderBottomWidth: 1,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: colors[theme].MAIN,
-  },
-  loginButton: {
-    fontSize: 16,
-    color: colors[theme].GRAY_600,
-  },
   tabletContainer: {
     flexDirection: 'row-reverse',
+    flex: 1,
+  },
+  contentContainer: {
     flex: 1,
   },
   testLayout: {
     flex: 1,
     backgroundColor: colors[theme].WHITE,
-    // padding: 30,
   },
   navItemTablet: {
     height: 60,
