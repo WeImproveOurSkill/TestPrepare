53864a6f9944d96c5af01cc5a1f702a18b9c0e13
benidene <dlgusehd09@naver.com>
2025-03-05 18:37:46 +0900

    <fe> <style> : Applying icons using react-native-vector-icons

--- front/src/screens/components/ExamHeader.tsx
+++ front/src/screens/components/ExamHeader.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Pressable} from 'react-native';
 import { ScaledSheet } from 'react-native-size-matters';
 import { EdgeInsets, useSafeAreaInsets } from 'react-native-safe-area-context';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import ChatIcon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
 import { colors } from '../../constants/colors';
 import useThemeStore, { themeMode } from '../../store/useThemeStore';
 
@@ -12,17 +14,24 @@ function ExamHeader() {
   const insets = useSafeAreaInsets();
   const {theme} = useThemeStore();
   const styles = styling(theme, insets);
+  const navigation = useNavigation();
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
 
   return (
     <View style={styles.container}>
         <View style={styles.header}>
           <View style={styles.headerButtonContainer}>
-            <Icon name="arrow-back-ios" style={styles.icon} />
+            <Pressable onPress={handleGoBack}>
+              <MaterialIcons name="arrow-back-ios" style={styles.icon} />
+            </Pressable>
           </View>
           <View style={styles.headerButtonContainer}>
-            <ChatIcon name="chatbox" style={styles.icon} />
-            <Icon name="arrow-back-ios" style={styles.icon} />
-            <Icon name="arrow-back-ios" style={styles.icon} />
+            <MaterialCommunityIcons name="comment-question-outline" style={styles.GptIcon} />
+            <FontAwesomeIcons name="sticky-note-o" style={styles.icon} />
+            <MaterialCommunityIcons name="calculator-variant-outline" style={styles.CalculatorIcon} />
           </View>
         </View>
     </View>
@@ -49,6 +58,17 @@ const styling = (theme: themeMode, insets: EdgeInsets) => ScaledSheet.create({
   icon: {
     color: colors[theme].MAIN,
     fontSize: '24@mvs0.3',
+    paddingLeft: '14@ms',
+  },
+  GptIcon: {
+    color: colors[theme].MAIN,
+    fontSize: '24@mvs0.3',
+    paddingLeft: '14@ms',
+    transform: [{ scaleY: 1.1 }],
+  },
+  CalculatorIcon: {
+    color: colors[theme].MAIN,
+    fontSize: '30@mvs0.3',
     paddingLeft: '14@ms',
   },
 });
