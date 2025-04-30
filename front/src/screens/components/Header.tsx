98f0818bef99012298642c31ced0c70ab4387c53
benidene <dlgusehd09@naver.com>
2025-04-30 11:00:04 +0900

    <fe> <feat>: Asynchronous import of user name and conditional display of login button - Add fetchUserName function to update status using getEncryptStorage function to get username - Implement conditional rendering to display the login button if the user name is not present, and to display the user name if the name is present - In Style, resize the text of the login button and change the bottom boundary color

--- front/src/screens/components/Header.tsx
+++ front/src/screens/components/Header.tsx
@@ -1,20 +1,62 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Pressable, Text, View } from 'react-native';
 import { EdgeInsets, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { ScaledSheet } from 'react-native-size-matters';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import useThemeStore, { themeMode } from '../../store/useThemeStore';
 import { colors } from '../../constants/colors';
+import { getEncryptStorage, UserKey } from '../../util/encryptStorage';
+import { RootStackParamList } from '../../navigation/RootStackNavigator';
 
 
 function Header() {
   const { theme } = useThemeStore(); // 현재 테마 가져오기
   const insets = useSafeAreaInsets();
   const styles = styling(theme, insets); // 테마별 스타일 적용
+  const [userName, setUserName] = useState<string | null>(null); // 사용자 이름을 저장할 상태 변수
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+
+  async function fetchUserName() {
+    try {
+      const data = await getEncryptStorage(UserKey);
+
+      if (data && data.username) {
+        const originalName = data.username;
+        // '_'가 포함되어 있는지 확인하고, 포함되어 있다면 '_' 앞부분만 사용
+        const processedName = originalName.includes('_')
+          ? originalName.split('_')[0]
+          : originalName;
+        setUserName(processedName);
+      } else {
+        setUserName(''); // 예: 빈 문자열로 설정
+      }
+    } catch (error) {
+      setUserName(''); // 오류 발생 시 빈 문자열 설정
+    }
+  }
+
+  useEffect(() => {
+    fetchUserName();
+  }, []);
+
+  const backToLoginScreen = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'AuthHome' }],
+    });
+  };
 
   return (
       <View style={[styles.header]}>
         <Text style={styles.title}>기한82</Text>
-        <Text style={styles.loginButton}>로그인</Text>
+        {userName === '' ? (
+          <Pressable onPress={backToLoginScreen}>
+            <Text style={styles.loginButtonText}>로그인</Text>
+          </Pressable>
+        ) : (
+            <Text style={styles.loginButtonText}>{userName}님</Text>
+        )}
       </View>
   );
 }
@@ -28,7 +70,7 @@ const styling = (theme: themeMode, insets: EdgeInsets) => ScaledSheet.create({
     paddingVertical: '10@vs',
     paddingHorizontal: '20@ms',
     backgroundColor: colors[theme].WHITE,
-    borderBottomColor: colors[theme].GRAY_200,
+    borderBottomColor: colors[theme].GRAY_400,
     borderBottomWidth: 1,
   },
   title: {
@@ -36,8 +78,8 @@ const styling = (theme: themeMode, insets: EdgeInsets) => ScaledSheet.create({
     fontWeight: '600',
     color: colors[theme].MAIN,
   },
-  loginButton: {
-    fontSize: '16@ms0.3',
+  loginButtonText: {
+    fontSize: '20@ms0.3',
     color: colors[theme].GRAY_600,
   },
 });
